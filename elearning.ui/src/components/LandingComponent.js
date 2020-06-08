ce619462c232ceef3d1ec884314315314cc9c193
Avishek Arora <avishek.arora@synapse.com>
2020-06-08 20:54:10 +0530

    Added basic components.

--- elearning.ui/src/components/LandingComponent.js
+++ elearning.ui/src/components/LandingComponent.js
@@ -1,14 +1,14 @@
 import React, {Component} from 'react'
 
-class Register extends Component{
+class Landing extends Component{
     constructor(props){
         super(props);
     }
 
     render(){
-        return <><h1>Register</h1></>
+        return <></>
     }
 }
 
 
-export default Register;
+export default Landing;
